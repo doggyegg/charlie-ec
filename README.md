95b4b0377ac98e4926fd8e98aabb718c67a53d86
clh <chenlihuai@chexiao.co>
2024-07-12 14:22:58 +0800

    config(custom): readme补充

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# charlie-ec
+# 项目简介
 
-Charlie 电商项目，包含管理端，客户端（PC/移动），服务端代码
+Charlie个人电商项目，准备个人商用，整个项目使用Monorepo架构，包含管理端，客户端（PC/移动），服务端代码
+
+# 项目结构
 
 base：公共组件
 utils：公共方法
 admin：后台管理端 技术选型：vue3 vue-router pinia element-plus
 customer：客户PC端 技术选型：react react-router mobx antd
-mobile:客户移动端(h5,小程序) 技术选型：Uniapp
-service：服务端 技术选型：nest.js
+mobile:客户移动端(h5,小程序) 技术选型：uniapp + vue3
+service：服务端 技术选型：nest.js mysql typeorm
 source：部分第三方组件库源码，用于学习和debugger
+
+# 规范
+
+eslint + prettier做代码格式规范
+husky + lint-staged + commit-lint + cz 做提交规范
--- packages/_utils/src/util.ts
+++ packages/_utils/src/util.ts
@@ -9,12 +9,12 @@ export default {
 	Tree2Flat(treeData: Array<ITreeItem>): Array<ITreeItem> {
 		if (!Array.isArray(treeData)) return treeData;
 
-		const result = [];
-		const recurFunc = (tree, total) => {
+		const result: Array<ITreeItem> = [];
+		const recurFunc = (tree: Array<ITreeItem>, total: Array<ITreeItem>) => {
 			tree.forEach(item => {
 				total.push(item);
-				if (item.children?.length > 0) {
-					recurFunc(item.children, total);
+				if ((item.children?.length as any) > 0) {
+					recurFunc(item.children as any, total);
 				}
 			});
 		};
@@ -30,7 +30,7 @@ export default {
 	 * @return treeData:多棵树树结构
 	 */
 	Flat2Tree(flatData: Array<ITreeItem>): Array<ITreeItem> {
-		const result = [];
+		const result: ITreeItem[] = [];
 		const flatMap = new Map();
 
 		flatData.forEach(item => {
@@ -48,5 +48,46 @@ export default {
 			}
 		});
 		return result;
+	},
+
+	/**
+	 * 深度遍历树
+	 * @params treeData:树数据  cb:每次执行的回调函数
+	 * @return void
+	 */
+	traverseTreeDFC(treeData: Array<ITreeItem>, cb: Function, childrenFlag: string = 'children') {
+		if (!treeData || typeof cb !== 'function') return treeData;
+		if (!Array.isArray(treeData) && typeof treeData === 'object') {
+			treeData = [treeData];
+		}
+		treeData.forEach(item => {
+			cb(item);
+			if ((item[childrenFlag] as Array<ITreeItem>).length > 0) {
+				this.traverseTreeDFC(item[childrenFlag], cb, childrenFlag);
+			}
+		});
+	},
+	/**
+	 * 广度遍历树
+	 * @params treeData cb:每次执行的回调函数
+	 * @return void
+	 */
+	traverseTreeBFC(treeData: Array<ITreeItem>, cb: Function, childrenFlag: string = 'children') {
+		if (!treeData || typeof cb !== 'function') return treeData;
+		if (!Array.isArray(treeData) && typeof treeData === 'object') {
+			treeData = [treeData];
+		}
+		while (treeData.length > 0) {
+			const node = treeData.shift();
+
+			if (node) {
+				cb(node);
+			}
+			if ((node[childrenFlag] as Array<ITreeItem>)?.length > 0) {
+				(node[childrenFlag] as Array<ITreeItem>).forEach(node1 => {
+					treeData.push(node1);
+				});
+			}
+		}
 	}
 };
