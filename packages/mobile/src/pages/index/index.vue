5da5fe3574c9b60a665b02b59c7142be313a228c
clh <chenlihuai@chexiao.co>
2024-10-28 10:21:26 +0800

    feat:重新初始化移动端项目

--- packages/mobile/src/pages/index/index.vue
+++ packages/mobile/src/pages/index/index.vue
@@ -1,10 +1,5 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{ title }}222</text>
-		</view>
-	</view>
+	<view class="content"> Home </view>
 </template>
 
 <script>
